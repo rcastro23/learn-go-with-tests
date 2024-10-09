da0ea38abb5049961087469fe67563086cd1d517
rcastro23 <ruidpcastro@gmail.com>
2024-10-10 00:40:46 +0100

    Add support for french language

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -3,18 +3,27 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
+const french = "French"
+
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
 	}
-	return englishHelloPrefix + name
+
+	return prefix + name
 }
 
 func main() {
