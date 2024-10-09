ba1d5fe2277005aeb3e61746f41d535b5234b282
rcastro23 <ruidpcastro@gmail.com>
2024-10-10 00:34:24 +0100

    Add support for spanish language

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -4,13 +4,18 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("empty strings default to 'world'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", spanish)
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
