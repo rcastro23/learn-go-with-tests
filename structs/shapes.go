73eafcd4863946e261eafbfbdc6cfe8745226738
rcastro23 <ruidpcastro@gmail.com>
2024-10-11 00:10:29 +0100

    Write the minimal amount of code for the test to run and check the failing test output

--- structs/shapes.go
+++ structs/shapes.go
@@ -23,6 +23,11 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
