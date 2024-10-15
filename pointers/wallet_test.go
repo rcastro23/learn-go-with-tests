d3144c0b5805088ec72586fe32cdda7b7ec75355
rcastro23 <ruidpcastro@gmail.com>
2024-10-15 23:01:09 +0100

    Write the test first

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,22 +1,36 @@
 package pointers
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	got := wallet.Balance()
+		wallet.Deposit(Bitcoin(10))
 
-	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
+		got := wallet.Balance()
 
-	want := Bitcoin(20)
+		want := Bitcoin(20)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
