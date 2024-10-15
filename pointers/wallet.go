3dfd24ddd0d8137b0d25dd26ded560ada3c70035
rcastro23 <ruidpcastro@gmail.com>
2024-10-15 22:47:21 +0100

    Write enough code to make it pass

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,13 +1,18 @@
 package pointers
 
-type Wallet struct{}
+import "fmt"
 
-func (w Wallet) Deposit(amount int) {
-
+type Wallet struct {
+	balance int
 }
 
-func (w Wallet) Balance() int {
-	return 0
+func (w *Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
 }
 
 func main() {
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,6 +1,9 @@
 package pointers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
@@ -8,6 +11,9 @@ func TestWallet(t *testing.T) {
 	wallet.Deposit(10)
 
 	got := wallet.Balance()
+
+	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
+
 	want := 10
 
 	if got != want {
