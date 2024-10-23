0ac994ebef7fafd30732ec31f6a7ac550465d851
rcastro23 <ruidpcastro@gmail.com>
2024-10-23 22:42:23 +0100

    Refactor

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -7,3 +7,21 @@ func walk(x interface{}, fn func(input string)) {
 	field := val.Field(0)
 	fn(field.String())
 }
+
+/*func TestWalk(t *testing.T) {
+
+	expected := "Chris"
+	var got []string
+
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if got[0] != expected {
+		t.Errorf("got %q, want %q", got[0], expected)
+	}
+}*/
