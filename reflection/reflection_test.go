54779cf385ddca7bf7350da3fb9e8bf4942bc0b3
rcastro23 <ruidpcastro@gmail.com>
2024-10-23 22:25:33 +0100

    Write the minimal amount of code for the test to run and check the failing test output

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,36 +1,23 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
 func TestWalk(t *testing.T) {
 
-	cases := []struct {
-		Name          string
-		Input         interface{}
-		ExpectedCalls []string
-	}{
-		{
-			"struct with one string field",
-			struct {
-				Name string
-			}{"Chris"},
-			[]string{"Chris"},
-		},
-	}
+	expected := "Chris"
+	var got []string
 
-	for _, test := range cases {
-		t.Run(test.Name, func(t *testing.T) {
-			var got []string
-			walk(test.Input, func(input string) {
-				got = append(got, input)
-			})
+	x := struct {
+		Name string
+	}{expected}
 
-			if !reflect.DeepEqual(got, test.ExpectedCalls) {
-				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
-			}
-		})
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if len(got) != 1 {
+		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
 	}
 }
