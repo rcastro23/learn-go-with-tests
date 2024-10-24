3110bfe299cc26254cd75f46fcacaf794e4b4c8b
rcastro23 <ruidpcastro@gmail.com>
2024-10-24 10:29:31 +0100

    Write the test first

--- context/context.go
+++ context/context.go
@@ -5,10 +5,13 @@ import (
 	"net/http"
 )
 
+// Store fetches data.
 type Store interface {
 	Fetch() string
+	Cancel()
 }
 
+// Server returns a handler for calling Store.
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, store.Fetch())
